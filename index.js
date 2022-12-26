08a4d602517bccba2c6c609f71ea2880f9c78526
Arman Patel <89066881+Arman6117@users.noreply.github.com>
2022-12-26 13:55:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+let drumButtons = document.querySelectorAll(".drum").length;
+let i = 0;
+
+while (i < drumButtons) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    let drums = this.innerHTML;
+   makesound(drums)
+   buttonAnimation(drums)
+    
+  });
+  i++;
+}
+
+document.addEventListener("keydown", function(event){
+  makesound(event.key)
+  buttonAnimation(event.key)
+})
+
+function makesound(key) {
+
+  switch (key) {
+    case "w":
+      let audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+
+      break;
+
+    case "a":
+     let tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+
+      break;
+    case "s":
+     let tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+
+      break;
+    case "d":
+     let tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+
+      break;
+    case "j":
+     let snare = new Audio("sounds/snare.mp3");
+      snare.play();
+
+      break;
+    case "k":
+     let crash = new Audio("sounds/crash.mp3");
+      crash.play();
+
+      break;
+    case "o":
+     let kick  = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+
+      break;
+
+    default: 
+      console.log(this.innerHTML)
+      break;
+  }
+}
+
+function buttonAnimation(key) {
+  let btnAni = document.querySelector("."+key)
+   btnAni.classList.add("pressed")
+   setTimeout(function () {
+    btnAni.classList.remove("pressed")
+   }, 100)
+}
